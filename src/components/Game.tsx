829ddbe1ca1c8b65d7152f90cf9bb171fc1937a1
Michał Bryk <galeriakoloru90210@gmail.com>
2023-03-18 16:07:03 +0100

    create types and initial values of snake and food position

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -3,11 +3,35 @@ import { SafeAreaView, StyleSheet } from "react-native";
 import { PanGestureHandler } from "react-native-gesture-handler";
 import { Colors } from "../styles/colors";
 
+//external imports
+import { GestureEventType } from "../types/types";
+
+const SNAKE_INITIAL_POSITION = [{ x: 5, y: 5 }];
+const FOOD_INITIAL_POSITION = { x: 5, y: 20 };
+const GAME_BOUNDS = { xMin: 0, xMax: 35, yMin: 0, yMax: 63 };
+const MOVE_INTERVAL = 50;
+const SCORE_INCREMENT = 10;
+
 export default function Game(): JSX.Element {
-  const handleGesture = (event: any) => {};
+  const handleGesture = (event: GestureEventType) => {
+    const { translationX, translationY } = event.nativeEvent;
+    if (Math.abs(translationX) > Math.abs(translationY)) {
+      if (translationX > 0) {
+        // moving right
+      } else {
+        // moving left
+      }
+    } else {
+      if (translationY > 0) {
+        // moving down
+      } else {
+        // moving up
+      }
+    }
+  };
 
   return (
-    <PanGestureHandler>
+    <PanGestureHandler onGestureEvent={handleGesture}>
       <SafeAreaView style={styles.container}></SafeAreaView>
     </PanGestureHandler>
   );
